f1f870ae5259fb71f48e91aa92eb87d0301ce93e
SP099 <sushanthpatil146@gmail.com>
2026-04-22 09:18:43 +0530

    feat(i2c): add I2C driver skeleton files

---
+++ stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h
@@ -0,0 +1,12 @@
+/*
+ * stm32f407xx_i2c_driver.h
+ *
+ *  Created on: Apr 22, 2026
+ *      Author: Lenovo
+ */
+
+#ifndef INC_STM32F407XX_I2C_DRIVER_H_
+#define INC_STM32F407XX_I2C_DRIVER_H_
+
+
+#endif /* INC_STM32F407XX_I2C_DRIVER_H_ */
